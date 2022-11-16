df02328fdb046a89a7230c39cdca8852845a6251
nuneung1 <54104048+nuneung1@users.noreply.github.com>
2022-11-16 10:55:51 +0700

    Create wordlist.py
    country vocab by 1

---
+++ wordlist.py
@@ -0,0 +1,11 @@
+""" word list"""
+def words():
+    """ words in game """
+    country_e = ["Brazil", "Laos", "China", "Egypt", "India", "Peru", "Russia", "Serbia", "Turkey", "Fiji", "Nepal",\
+"Libya", "Kenya", "Iran", "Italy", "Japan", "Korea", "France", "Cuba", "Canada"]
+    country_n = ["Bhutan", "Algeria", "Colombia", "Dominica", "Ecuador", "Georgia", "Hungary", "Lebanon", "Malaysia", \
+"Netherlands", "Nicaragua", "Paraguay", "Philippines", "Slovakia", "Switzerland", "Uruguay", "Venezuela", "Suriname", \
+"Tunisia","Sri Lanka"]
+    country_h = ["Afghanistan", "Azerbaijan", "Bangladesh", "Botswana", "Djibouti", "Kyrgyzstan", "Liechtenstein", \
+"Luxembourg", "Madagascar", "Mauritania", "Saudi Arabia", "Tajikistan", "Turkmenistan", "United Arab Emirates", \
+"United States of America", "United Kingdom", "Zimbabwe", "Palestine State", "Papua New Guinea", "Kazakhstan"]
