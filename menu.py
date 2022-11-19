700e7988da3d7c6b8a5558ddbc583d5adc327618
nuneung1 <gyugg21@gmail.com>
2022-11-19 11:07:23 +0700

    sth

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 """MAIN MENU"""
 import pygame
 import sys
+import button
 from pygame.locals import *
 #menu
 def menu():
@@ -21,62 +22,40 @@ def menu():
     pygame.display.update()
 
     #Fonts
-    #Dimbo
     game_font = pygame.font.Font('fonts/Dimbo Regular.ttf', 80)
     letter_font = pygame.font.Font('fonts/Dimbo Regular.ttf', 40)
 
+    #text in menu
+    #title
     title = "Hang That Head"
     title_text = game_font.render(title, True, black)
     title_text_rect = title_text.get_rect(center=(WIDTH//2,title_text.get_height()//2+20))
+
     #Select mode
     select = "Select Mode:"
     select_text = letter_font.render(select, True, gray)
     select_text_rect = select_text.get_rect(center=(WIDTH//2,title_text.get_height()//2+100))
+
     #button
+    #load image
+    exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+
+    #create button instances
+    exit_button = button.Button(450, 200, exit_img, 0.8)
+
 
 
     while True:
         screen.fill(yellow)
         screen.blit(title_text, title_text_rect)
         screen.blit(select_text, select_text_rect)
+        if exit_button.draw(screen):
+            print("Press")
         pygame.display.update()
         for event in pygame.event.get():
-            pos = pygame.mouse.get_pos
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
         clock.tick(50)
+        pygame.display.update()
 
-
-
-
-
-class button():
-    def __init__(self, color, x,y,width,height, text=''):
-        self.color = color
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-
-    def draw(self,win,outline=None):
-        #Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
-            
-        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
-        
-        if self.text != '':
-            font = pygame.font.SysFont('comicsans', 60)
-            text = font.render(self.text, 1, (0,0,0))
-            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
-
-    def isOver(self, pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
-            
-        return False
-menu()
